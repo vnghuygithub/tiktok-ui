34f9dda35a8ce329f23e004693fe9e33dc3b977d
Vuong Q. Huy <119883477+vnghuygithub@users.noreply.github.com>
2023-07-09 11:46:10 +0700

    a

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -8,6 +8,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -19,7 +20,7 @@ function Header() {
     const [searchResults, setSearchResults] = useState([]);
     useEffect(() => {
         setTimeout(() => {
-            setSearchResults([1, 2, 3]);
+            setSearchResults([]);
         }, 0);
     }, []);
 
@@ -65,7 +66,9 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </header>
     );
